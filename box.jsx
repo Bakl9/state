3ba0a15adb99d8058787f60d8600980e65c6eb68
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-11-11 22:34:15 +0300

    Update box.jsx
    input

--- box.jsx
+++ box.jsx
@@ -1,14 +1,18 @@
+const Box = () => {
 
-const box = () => {
-
-    const [count,stateCount] = useState( 1)
-
-    const decrement = () => {
-        stateCount(count + 1)
-    }
+    const [count, setCount] = useState( 0)
+    const [value, setValue] = useState( 'текст инпута')
 
     const increment = () => {
-        stateCount(count - 1)
+        setCount( count +1 )
+    }
+
+    const decrement = () => {
+        setCount( count -1 )
+    }
+
+    const input = (event) => {
+        setValue(event.target.value)
     }
 
     return (
@@ -16,6 +20,8 @@ const box = () => {
             <button onClick={increment}>increment</button>
             <button onClick={decrement}>decrement</button>
             <h1>{count}</h1>
+            <h1>{value}</h1>
+            <input onInput={input}/>
         </div>
     )
 }
