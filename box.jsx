655c01085eefee62a10991b1800158d2b6e4f6ee
Makarov ilya <74796903+Bakl9@users.noreply.github.com>
2021-11-11 20:06:30 +0300

    Create box.jsx

---
+++ box.jsx
@@ -0,0 +1,21 @@
+
+const Box = () => {
+
+    const [count,stateCount] = useState( 1)
+
+    const decrement = () => {
+        stateCount(count + 1)
+    }
+
+    const increment = () => {
+        stateCount(count - 1)
+    }
+
+    return (
+        <div>
+            <button onClick={increment}>increment</button>
+            <button onClick={decrement}>decrement</button>
+            <h1>{count}</h1>
+        </div>
+    )
+}
